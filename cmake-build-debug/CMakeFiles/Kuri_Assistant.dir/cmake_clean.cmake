80d05d6d94826de0ec29b2bce452adf928dfbac0
ngot23 <levuquang2k1@gmail.com>
2020-12-20 13:48:54 +0700

    read .gitignore

---
+++ cmake-build-debug/CMakeFiles/Kuri_Assistant.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Kuri_Assistant.dir/Kuri_Assistant/ACore.cpp.o"
+  "CMakeFiles/Kuri_Assistant.dir/Kuri_Assistant/Dictionary.cpp.o"
+  "CMakeFiles/Kuri_Assistant.dir/Kuri_Assistant/Kuri_Assistant.cpp.o"
+  "CMakeFiles/Kuri_Assistant.dir/Kuri_Assistant/Ultility.cpp.o"
+  "CMakeFiles/Kuri_Assistant.dir/Kuri_Assistant/main.cpp.o"
+  "Kuri_Assistant"
+  "Kuri_Assistant.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Kuri_Assistant.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Kuri_Assistant.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Kuri_Assistant.
+# This may be replaced when dependencies are built.
